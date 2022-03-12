824c756d8539265e2dec134fbc6a8eb199b1e91f
MBlev <mb.lev17@gmail.com>
2022-03-12 18:18:15 -0500

    Pages added to sidebar

---
+++ src/layouts/sidebar/Sidebar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { FaBars, FaBorderAll } from 'react-icons/fa';
+
+import { NavLink } from 'react-router-dom';
+
+import './Sidebar.css';
+import logo from '../../assets/logo.png' 
+
+class Sidebar extends React.Component {
+    render() {
+      return (
+        <div className="sidebar">
+            <img className='logo' src={logo} alt='Ravn logo'></img>
+            <nav>
+                <ul>
+                    <li><NavLink className={({isActive}) => isActive ? "link-active body-m-bold" : "link body-m-bold"} to="/">
+                        <FaBorderAll className='icon'/>  DASHBOARD
+                    </NavLink></li>
+                    <li><NavLink className={({isActive}) => isActive ? "link-active body-m-bold" : "link body-m-bold"} to="/my-task">
+                        <FaBars className='icon' />  MY TASK
+                    </NavLink></li>
+                </ul>
+            </nav>
+        </div>
+      );
+    }
+}
+
+export default Sidebar;
