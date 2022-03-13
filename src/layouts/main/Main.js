2075fb804a58d78ac0b5bf62a2ecdaf3b586f351
MBlev <mb.lev17@gmail.com>
2022-03-12 20:03:27 -0500

    Main with base structure

--- src/layouts/main/Main.js
+++ src/layouts/main/Main.js
@@ -1,26 +1,27 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Dashboard from '../../pages/dashboard/Dashboard';
 import MyTask from '../../pages/my-task/MyTask';
+import Header from '../header/Header';
+
+import Sidebar from "../sidebar/Sidebar";
+
+import './Main.css';
 
 function Main() {
     return (
-        <div className="wrapper">
-            <h1>Ravn Challenge</h1>
-            <BrowserRouter>
-                <nav>
-                    <ul>
-                        <li><Link to="/dashboard">Dashboard</Link></li>
-                        <li><Link to="/my-task">My Task</Link></li>
-                    </ul>
-                </nav>
-                <Routes>
-                    <Route path="/" element={<Dashboard />}/>
-                    <Route path="/dashboard" element={<Dashboard />}/>
-                    <Route path="/my-task" element={<MyTask />}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <BrowserRouter>
+            <div className="wrapper">
+                <Sidebar />
+                <div className='content'>
+                    <Header />
+                    <Routes>
+                        <Route exact path="/" element={<Dashboard />}/>
+                        <Route exact path="/my-task" element={<MyTask />}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
