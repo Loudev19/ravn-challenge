b959afe689aa96f4af1d1b8763a92f431a20b634
MBlev <mb.lev17@gmail.com>
2022-03-13 01:43:14 -0500

    Fixed styles in switch buttons

--- src/components/switch-views/SwitchViews.js
+++ src/components/switch-views/SwitchViews.js
@@ -2,14 +2,16 @@ import { NavLink } from 'react-router-dom';
 
 import { FaBars, FaBorderAll } from 'react-icons/fa';
 
-function SwitchViews(props) {
+import './SwitchViews.css';
+
+function SwitchViews() {
     return (
         <div className="options">
-            <NavLink className={({isActive}) => isActive ? "switch-active" : "switch"} to="/">
-                <FaBorderAll />
-            </NavLink>
             <NavLink className={({isActive}) => isActive ? "switch-active" : "switch"} to="/my-task">
                 <FaBars />
+            </NavLink>
+            <NavLink className={({isActive}) => isActive ? "switch-active" : "switch"} to="/">
+                <FaBorderAll />
             </NavLink>
         </div>
     );
