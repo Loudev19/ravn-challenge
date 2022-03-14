4b3e73eca6377e4bcf9653196a0df217b417b47d
MBlev <mb.lev17@gmail.com>
2022-03-13 22:29:32 -0500

    Task card constants

---
+++ src/constants/TaskValues.js
@@ -0,0 +1,15 @@
+export const POINTS = new Map([
+    ['ZERO', 0],
+    ['UNO', 1],
+    ['DOS', 2],
+    ['FOUR', 4],
+    ['EIGHT', 8]
+]);
+
+export const STATUS = new Map([
+    ['BACKLOG', 'Backlog'],
+    ['CANCELLED', 'Cancelled'],
+    ['DONE', 'Done'],
+    ['IN_PROGRESS', 'In Progress'],
+    ['TODO', 'To Do']
+]);
