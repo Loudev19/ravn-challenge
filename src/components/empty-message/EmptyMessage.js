b9493f57eac4492d9960cac75cf0b8869d4819a3
MBlev <mb.lev17@gmail.com>
2022-03-15 00:00:27 -0500

    Empty message component created

---
+++ src/components/empty-message/EmptyMessage.js
@@ -0,0 +1,9 @@
+import './EmptyMessage.css'
+
+export default function EmptyMessage() {
+    return (
+        <div className='empty-box body-m-regular'>
+            There is no elements yet
+        </div>
+    );
+}
